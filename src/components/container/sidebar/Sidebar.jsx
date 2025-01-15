7793eb1e4f126515a9b6438d1e712f0418028287
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2025-01-16 04:05:31 +0530

    fix

--- src/components/container/sidebar/Sidebar.jsx
+++ src/components/container/sidebar/Sidebar.jsx
@@ -33,7 +33,7 @@ const Sidebar = () => {
         </span>
         {showModal && (
           <div
-            className={`absolute top-[120px] left-[25%] bg-white border shadow-lg p-4 rounded ${
+            className={`absolute top-[120px] left-[25%] border shadow-lg p-4 rounded ${
               theme === "dark"
                 ? "bg-[#3a3a3a] text-white"
                 : "bg-[#f9f9f9] text-black"
